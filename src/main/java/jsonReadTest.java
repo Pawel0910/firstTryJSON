dabe08fe6162b97aa35e7ae1835264658ce9e322
pai-50 <187724@edu.p.lodz.pl>
2016-02-24 23:14:00 +0100

    First commit with JSON and XML

---
+++ src/main/java/jsonReadTest.java
@@ -0,0 +1,39 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.Iterator;
+
+
+/**
+ * Created by Kielan on 23.02.2016.
+ */
+public class jsonReadTest {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            Object obj = parser.parse(new FileReader("file1.txt"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            String name = (String) jsonObject.get("Name");
+            String author = (String) jsonObject.get("Author");
+            JSONArray companyList = (JSONArray) jsonObject.get("Company List");
+
+            System.out.println("Name: " + name);
+            System.out.println("Author: " + author);
+            System.out.println("\nCompany List:");
+            Iterator<String> iterator = companyList.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
